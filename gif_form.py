aa18f99396dc5728d73bb56a2827f23d7951ea1d
hadro <josh.hadro@gmail.com>
2015-12-12 18:28:43 -0500

    Handling GET and POST

--- gif_form.py
+++ gif_form.py
@@ -10,11 +10,16 @@ app = Flask(__name__)
 def my_form():
     return render_template("gif.html")
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def my_form_post():
-	uuid = request.form['text']
-	title = animated_gif_function.create_gif(uuid)
-	return send_file('./gifs/'+title+'.gif', mimetype='image/gif')
+	if request.method == 'GET':	
+		uuid = request.form['text']
+		title = animated_gif_function.create_gif(uuid)
+		return send_file('./gifs/'+title+'.gif', mimetype='image/gif')
+	if request.method == 'POST':
+		uuid = request.form['text']
+		title = animated_gif_function.create_gif(uuid)
+		return send_file('./gifs/'+title+'.gif', mimetype='image/gif')
 
 if __name__ == '__main__':
     app.debug = True
