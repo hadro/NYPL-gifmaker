d15670665f5a435fad54426eb85b2f9fda2a4c96
hadro <josh.hadro@gmail.com>
2015-12-13 17:25:53 -0500

    Showing previous gif versions on the templates

--- gif_form.py
+++ gif_form.py
@@ -3,13 +3,19 @@ from flask import request
 from flask import render_template
 from flask import send_file
 import animated_gif_function
+import glob
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/')
 def my_form():
-        return render_template("gif.html")
+	gif_files = []
+	for file in glob.glob('./static/gifs/*.gif'):
+		gif_files.append(file)
+	gif_files.sort(key=os.path.getmtime)
+	return render_template("gif.html", previous_gifs=gif_files[-3:])
 
 @app.route('/', methods=['POST'])
 def my_form_post():
@@ -18,7 +24,13 @@ def my_form_post():
 	title = animated_gif_function.create_gif(uuid, size)
 	if title[0] != False:
 		#return send_file('../public/gifs/'+title+'.gif', mimetype='image/gif')
-		return render_template('gif-return.html', gif_path=title)
+		gif_files = []
+		for file in glob.glob('./static/gifs/*.gif'):
+			gif_files.append(file)
+		gif_files.sort(key=os.path.getmtime)
+
+		print gif_files[-2:]
+		return render_template('gif-return.html', gif_path=title, previous_gifs=gif_files[-3:])
 	else:
 		return title[1]
 
